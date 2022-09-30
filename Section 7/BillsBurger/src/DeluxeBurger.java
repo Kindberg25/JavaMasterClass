8986ec9cd51611f729b177f2fa89c718efb8e234
Carl Kindberg <kindberg25@hotmail.com>
2022-09-30 11:46:57 +0200

    Initial commit

---
+++ Section 7/BillsBurger/src/DeluxeBurger.java
@@ -0,0 +1,17 @@
+/*
+For the second class, DeluxeBurger, there are no additional member variables, and the constructor accepts no parameters.
+Instead, the constructor creates a deluxe burger with all the fixings and chips and a drink for a base price of $19.10.
+The constructor can be configured in any way, as long as chips and drink are added for the total price just mentioned.
+In this class, the four(4) methods defined in the Hamburger class for including additional toppings must each be
+overridden so that a message is printed stating that no additional items can be added to a deluxe burger.
+ */
+
+public class DeluxeBurger extends Hamburger{
+
+    public DeluxeBurger() {
+
+    }
+
+
+
+}
